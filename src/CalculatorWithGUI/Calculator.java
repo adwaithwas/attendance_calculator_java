48c908adaade631913a0840b13962bacc47cd60a
waithium <waithium@proton.me>
2024-10-21 05:21:25 +0530

    .

---
+++ src/CalculatorWithGUI/Calculator.java
@@ -0,0 +1,14 @@
+package src.CalculatorWithGUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator{
+    Calculator(){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,25 @@ public class Main {
         //inputting attendance values
         int num; int den;
 
-        Scanner scan = new Scanner(System.in);
 
 
+
+        public void getAttendance(){
         //inputting values
-        System.out.print("Please enter your numerator: ");
-        num = scan.nextInt();
-        System.out.print("Please enter your denominator: ");
-        den = scan.nextInt();
-        scan.close();
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Please enter your numerator: ");
+            num = scan.nextInt();
+            System.out.print("Please enter your denominator: ");
+            den = scan.nextInt();
+            scan.close();
+        }
+
+
+        //checking if numbers are valid
+        if(num>den){
+            System.out.println("Invalid numbers!!");
+            getAttendance();
+        }
 
         Logic logic = new Logic(num, den);
         logic.LecReq();
